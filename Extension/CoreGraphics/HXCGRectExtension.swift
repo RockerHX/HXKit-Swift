a2f8afd202eea9d038244bbb866d995d4e75ba0a
RockerHX <RockerHX@gmail.com>
2017-03-31 11:31:42 +0800

    Add[Extension]: HXCGRectExtension

---
+++ Extension/CoreGraphics/HXCGRectExtension.swift
@@ -0,0 +1,54 @@
+//
+//  RectExtension.swift
+//  XRDLoanServices
+//
+//  Created by XRD.Andy on 2017/2/10.
+//  Copyright © 2017年 XRD.Andy. All rights reserved.
+//
+
+
+import UIKit
+
+
+extension CGRect {
+
+    var x: Double {
+        get {
+            return Double(origin.x)
+        }
+
+        set {
+            self.origin.x = CGFloat(newValue)
+        }
+    }
+
+    var y: Double {
+        get {
+            return Double(origin.y)
+        }
+
+        set {
+            self.origin.y = CGFloat(newValue)
+        }
+    }
+
+    var width: Double {
+        get {
+            return Double(size.width)
+        }
+
+        set {
+            self.size.width = CGFloat(newValue)
+        }
+    }
+
+    var height: Double {
+        get {
+            return Double(size.height)
+        }
+
+        set {
+            self.size.height = CGFloat(newValue)
+        }
+    }
+}
