c83840e6513b28a5655a6149e8ff1cdf0b315231
RockerHX <rockerhx@gmail.com>
2017-08-22 11:46:15 +0800

    Refactor[Extension-UIKit]: UIViewAnimator.swift
    1.Refactor Animator.
    2.Add SpringAnimator for spring animation.

--- Extension/UIKit/UIViewAnimator.swift
+++ Extension/UIKit/UIViewAnimator.swift
@@ -16,12 +16,13 @@ extension UIView {
         typealias Animations = () -> Void
         typealias Completion = (Bool) -> Void
 
-        fileprivate var success: Bool?
         fileprivate let duration: TimeInterval
+        fileprivate let delay: TimeInterval
+        fileprivate let options: UIViewAnimationOptions
 
         fileprivate var animations: Animations! {
             didSet {
-                UIView.animate(withDuration: duration, animations: animations) { (success) in
+                UIView.animate(withDuration: duration, delay: delay, options: options, animations: animations) { (success) in
                     self.completion?(success)
                     self.success = success
                 }
@@ -33,9 +34,12 @@ extension UIView {
                 completion?(success)
             }
         }
+        fileprivate var success: Bool?
 
-        init(duration: TimeInterval) {
+        init(duration: TimeInterval, delay: TimeInterval = 0, options: UIViewAnimationOptions = []) {
             self.duration = duration
+            self.delay = delay
+            self.options = options
         }
 
         public func animations(animations: @escaping Animations) -> Self {
@@ -49,4 +53,30 @@ extension UIView {
         }
     }
 
+}
+
+
+extension UIView {
+
+    class SpringAnimator: Animator {
+        
+        fileprivate var dampingRatio: CGFloat
+        fileprivate var velocity: CGFloat
+
+        override fileprivate var animations: Animations! {
+            didSet {
+                UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: dampingRatio, initialSpringVelocity: velocity, options: options, animations: animations) { (success) in
+                    self.completion?(success)
+                    self.success = success
+                }
+            }
+        }
+
+        init(duration: TimeInterval, delay: TimeInterval, usingSpringWithDamping dampingRatio: CGFloat, initialSpringVelocity velocity: CGFloat, options: UIViewAnimationOptions = []) {
+
+            self.dampingRatio = dampingRatio
+            self.velocity = velocity
+            super.init(duration: duration, delay: delay, options: options)
+        }
+    }
 }
