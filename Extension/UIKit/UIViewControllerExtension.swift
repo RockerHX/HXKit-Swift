83c6c7bd5e6691911ae8277274ff7a9a92a5845b
RockerHX <rockerhx@gmail.com>
2017-09-03 13:57:07 +0800

    Add[Extension-UIKit]: UIViewControllerExtension.swift

---
+++ Extension/UIKit/UIViewControllerExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewControllerExtension.swift
+//  Ding
+//
+//  Created by RockerHX on 2017/9/3.
+//  Copyright © 2017年 RockerHX. All rights reserved.
+//
+
+
+import UIKit
+
+
+public extension UIViewController {
+
+    @IBAction public func unwindToPreviousController(_ segue: UIStoryboardSegue) {}
+}
