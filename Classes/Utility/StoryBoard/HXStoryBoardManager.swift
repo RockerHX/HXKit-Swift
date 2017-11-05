954bbb61e51d63423f80ef962dd6eef1696c5aef
RockerHX <rockerhx@gmail.com>
2017-11-06 03:52:10 +0800

    Add[Class]: HXStoryBoardManager
    For load view controller from story board.

---
+++ Classes/Utility/StoryBoard/HXStoryBoardManager.swift
@@ -0,0 +1,41 @@
+//
+//  HXStoryBoardManager.swift
+//  HXKit-Swift
+//
+//  Created by ShiCang on 16/1/10.
+//  Copyright © 2016年 Cave. All rights reserved.
+//
+//  GitHub: https://github.com/rockerhx
+//
+
+
+import UIKit
+
+
+protocol HXStoryBoardProtocol {
+
+    associatedtype Element where Element: UIViewController
+
+    func instance() -> Self.Element
+}
+
+
+class HXStoryBoardManager {
+
+    static func navigaitonControllerWithIdentifier(identifier: String, storyBoard: String = "") -> UINavigationController? {
+        return self.viewController(withIdentifier: identifier, storyBoard: storyBoard) as? UINavigationController
+    }
+    
+    static func viewController(withClass viewController: UIViewController, storyBoard: String = "") -> UIViewController {
+        let identifier = String(describing: viewController)
+        return self.viewController(withIdentifier: identifier, storyBoard: storyBoard)
+    }
+    
+    static func viewController(withIdentifier identifier: String, storyBoard: String = "") -> UIViewController {
+        var storyBoardFileName = storyBoard
+        if storyBoard.count == 0 {
+            storyBoardFileName = identifier
+        }
+        return UIStoryboard(name: storyBoardFileName, bundle: Bundle.main).instantiateViewController(withIdentifier: identifier)
+    }
+}
