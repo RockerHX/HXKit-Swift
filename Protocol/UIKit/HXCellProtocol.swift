1792d3292948a187a017f964cd74374a8f35892d
RockerHX <rockerhx@gmail.com>
2018-03-06 10:19:39 +0800

    Add[Protocol]: HXCellProtocol
    For fast use cell identifier.

---
+++ Protocol/UIKit/HXCellProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  HXCellProtocol.swift
+//  News
+//
+//  Created by RockerHX on 2018/3/6.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+
+import UIKit
+
+
+protocol HXCellProtocol {
+    static func identifier() -> String
+}
+
+
+extension HXCellProtocol {
+
+    static func identifier() -> String {
+        return String(describing: self.self)
+    }
+}
+
