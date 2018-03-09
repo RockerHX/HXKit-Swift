5555185f05ab4ee8557a4d8ac4730ca0b9f7ae3d
RockerHX <rockerhx@gmail.com>
2018-03-09 16:38:42 +0800

    Add[Extionsion]: UIKit - HXCellExtension
    UITableViewCell & UICollectionViewCell add class method: identifier()
    for return cell identifier.

---
+++ Extension/UIKit/HXCellExtension.swift
@@ -0,0 +1,29 @@
+//
+//  HXCellExtension.swift
+//  News
+//
+//  Created by RockerHX on 2018/3/9.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+
+import UIKit
+
+
+extension UITableViewCell {
+
+    static func identifier() -> String {
+        return String(describing: self)
+    }
+
+}
+
+
+extension UICollectionViewCell {
+
+    static func identifier() -> String {
+        return String(describing: self)
+    }
+
+}
+
