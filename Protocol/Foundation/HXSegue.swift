218c88920bb1090500e08836a6fa2c13a003736f
RockerHX <rockerhx@gmail.com>
2018-03-15 17:19:54 +0800

    Update[Protocol]: HXSegue
    添加方法用于页面跳转：performSegue(onController controller: Element)

--- Protocol/Foundation/HXSegue.swift
+++ Protocol/Foundation/HXSegue.swift
@@ -10,14 +10,25 @@ import Foundation
 
 
 protocol HXSegue: RawRepresentable {
+
+    associatedtype Element where Element: UIViewController
+
     var identifier: String { get }
+    func performSegue(onController controller: Element)
+    
 }
 
 extension HXSegue {
+
     var identifier: String {
         get {
             return self.rawValue as! String
         }
     }
+
+    func performSegue(onController controller: Element) {
+        controller.performSegue(withIdentifier: self.identifier, sender: nil)
+    }
+
 }
 
