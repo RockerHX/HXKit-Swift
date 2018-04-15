fffdb380f385606e029a2f45b32588cd2e04156f
RockerHX <rockerhx@gmail.com>
2018-04-15 22:36:45 +0800

    Update[HXIBKit]: HXIBViewExtension
    Add shadow properties.

--- HXIBKit/Extension/HXIBViewExtension.swift
+++ HXIBKit/Extension/HXIBViewExtension.swift
@@ -67,4 +67,53 @@ extension UIView {
             layer.borderColor = UIColor.color(withRGBHex: newValue).cgColor
         }
     }
+
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+
 }
+
