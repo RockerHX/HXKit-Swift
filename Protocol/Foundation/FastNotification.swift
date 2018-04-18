e44338116015e6fd54f2a6038e22dc8f441f6a0d
RockerHX <rockerhx@gmail.com>
2018-04-18 17:10:39 +0800

    Add[Protocol]: FastNotification
    快速处理Notification。

---
+++ Protocol/Foundation/FastNotification.swift
@@ -0,0 +1,36 @@
+//
+//  FastNotification.swift
+//  Delivery
+//
+//  Created by RockerHX on 2018/4/18.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+
+import Foundation
+
+
+protocol FastNotification: RawRepresentable {
+
+    var value: String { get }
+    var notifacation: Notification { get }
+    var name: Notification.Name { get }
+
+}
+
+
+extension FastNotification {
+
+    var value: String {
+        return rawValue as! String
+    }
+
+    var notifacation: Notification {
+        return Notification(name: name)
+    }
+
+    var name: Notification.Name {
+        return Notification.Name(value)
+    }
+
+}
