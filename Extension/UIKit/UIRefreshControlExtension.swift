3ae6238f300371b9968c6f5dfefae5f875394061
RockerHX <rockerhx@gmail.com>
2018-06-24 20:10:20 +0800

    Add[Extension]: UIRefreshControlExtension
    For trigger begin refresh animation.

---
+++ Extension/UIKit/UIRefreshControlExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UIRefreshControlExtension.swift
+//  DressBeautiful
+//
+//  Created by RockerHX on 2018/6/24.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+
+import UIKit
+
+
+extension UIRefreshControl {
+
+    func beginAnimateRefreshing() {
+        guard let tableView = superview as? UITableView else { return }
+        beginRefreshing()
+        tableView.contentOffset = CGPoint(x: 0, y: tableView.contentOffset.y - frame.size.height)
+        sendActions(for: .valueChanged)
+    }
+
+}
+
