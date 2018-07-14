be861ebbb4af3645e822df6d8d99905a2a67f68e
RockerHX <rockerhx@gmail.com>
2018-07-14 11:49:26 +0800

    Add[Class]: HXNavigationController
    用于修复隐藏导航条或者自定义过导航条导致右滑失效的情况。

---
+++ Classes/UIKit/Navigation/HXNavigationController.swift
@@ -0,0 +1,41 @@
+//
+//  HXNavigationController.swift
+//  News
+//
+//  Created by RockerHX on 2018/4/2.
+//Copyright © 2018年 RockerHX. All rights reserved.
+//
+//  GitHub: https://github.com/rockerhx
+//
+
+
+import UIKit
+
+
+class HXNavigationController: UINavigationController {
+
+    // MARK: - View Controller Life Cycle -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configure()
+    }
+
+    // MARK: - Event Methods -
+    // MARK: - Public Methods -
+    // MARK: - Private Methods -
+    private func configure() {
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+}
+
+
+extension HXNavigationController: UIGestureRecognizerDelegate {
+
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+
+}
+
