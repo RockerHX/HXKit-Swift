22f48d06f194a83d37bc333ef937fdfeec6cbecc
RockerHX <rockerhx@gmail.com>
2018-07-21 20:28:27 +0800

    Add[Extension]: UIView.BlurEffect.swift
    增加毛玻璃效果。

---
+++ Extension/UIKit/UIView.BlurEffect.swift
@@ -0,0 +1,26 @@
+//
+//  UIView.BlurEffect.swift
+//  HXKit-Swift
+//
+//  Created by RockerHX on 2017/2/22.
+//  Copyright © 2017年 RockerHX. All rights reserved.
+//
+//  GitHub: https://github.com/rockerhx
+//
+
+
+import UIKit
+
+
+// MARK: - Instance Methods -
+extension UIView {
+
+    public func toBlur(style: UIBlurEffectStyle = .dark) {
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = bounds
+        addSubview(blurView)
+    }
+
+}
+
