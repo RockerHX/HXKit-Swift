29f1236ad7dd3169615ca90a5166fd75d21ed606
RockerHX <rockerhx@gmail.com>
2018-07-28 11:11:31 +0800

    Add[Class]: PlaceholderTextView.swift
    可以显示占位提示的TextView。

---
+++ Classes/UIKit/TextView/PlaceholderTextView.swift
@@ -0,0 +1,116 @@
+//
+//  PlaceholderTextView.swift
+//  FishHead
+//
+//  Created by RockerHX on 2018/7/28.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+
+import UIKit
+
+
+class PlaceholderTextView: UITextView {
+
+    // MARK: - IBInspectable Property -
+    @IBInspectable var placeholder: String = "" {
+        didSet {
+            placeholderLabel.text = placeholder
+            setNeedsDisplay()
+        }
+    }
+    @IBInspectable var placeholderColor: UIColor = .lightGray {
+        didSet {
+            placeholderLabel.textColor = placeholderColor
+            setNeedsDisplay()
+        }
+    }
+
+    // MARK: - Public Property -
+    public var placeholderAlignment: NSTextAlignment = .left {
+        didSet {
+            placeholderLabel.textAlignment = placeholderAlignment
+            setNeedsDisplay()
+        }
+    }
+    // MARK: - Private Property -
+    private var placeholderLabel = UILabel()
+
+    // MARK: - Override Methods -
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        if !placeholder.isEmpty {
+            placeholderLabel.text = placeholder
+            placeholderLabel.textColor = placeholderColor
+            placeholderLabel.textAlignment = placeholderAlignment
+        }
+        display()
+        super.draw(rect)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let labelSize = placeholderLabel.sizeThatFits(CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude))
+        placeholderLabel.frame = CGRect(x: 6, y: 6, width: labelSize.width, height: labelSize.height)
+    }
+
+    // MARK: - Override Methods -
+    // MARK: - Initialize Methods -
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    // MARK: - Configuration Methods -
+    private func configure() {
+        configuePlaceholder()
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange(notification:)), name: NSNotification.Name.UITextViewTextDidChange, object: self)
+    }
+
+    private func configuePlaceholder() {
+        placeholderLabel.backgroundColor = .clear
+        placeholderLabel.lineBreakMode = .byCharWrapping
+        placeholderLabel.numberOfLines = 0
+        placeholderLabel.font = font
+        addSubview(placeholderLabel)
+    }
+
+}
+
+
+// MARK: - Event Methods -
+extension PlaceholderTextView {
+
+    @objc private func textDidChange(notification: NSNotification) {
+        guard let _ = notification.object as? PlaceholderTextView else { return }
+        updateLayout()
+        display()
+    }
+
+}
+
+
+// MARK: - Public Methods -
+extension PlaceholderTextView {
+}
+
+
+// MARK: - Private Methods -
+extension PlaceholderTextView {
+
+    private func updateLayout() {
+        invalidateIntrinsicContentSize()
+        scrollRangeToVisible(selectedRange)
+    }
+
+    private func display() {
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+
+}
+
