9947d0e13215b6ebbf176ac8814dabd2d69682eb
RockerHX <rockerhx@gmail.com>
2018-09-07 16:31:40 +0800

    Add[Protocol]: AnyDecoder
    简化数据解析代码。

---
+++ Protocol/Foundation/AnyDecoder.swift
@@ -0,0 +1,28 @@
+//
+//  AnyDecoder.swift
+//  FishHead
+//
+//  Created by RockerHX on 2018/9/7.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+
+import Foundation
+
+
+protocol AnyDecoder {
+    func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T
+}
+
+
+extension JSONDecoder: AnyDecoder {}
+extension PropertyListDecoder: AnyDecoder {}
+
+
+extension Data {
+
+    func decoded<T: Decodable>(using decoder: AnyDecoder = JSONDecoder()) throws -> T {
+        return try decoder.decode(T.self, from: self)
+    }
+
+}
