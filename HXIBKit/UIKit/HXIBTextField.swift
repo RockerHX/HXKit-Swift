c1ac779086877275c0c3b3cd7f6caf343d1832d4
RockerHX <rockerhx@gmail.com>
2018-12-17 15:31:13 +0800

    Create HXIBTextField.swift

---
+++ HXIBKit/UIKit/HXIBTextField.swift
@@ -0,0 +1,16 @@
+//
+//  HXIBTextField.swift
+//  HXKit-Swift
+//
+//  Created by RockerHX on 2018/12/17.
+//  Copyright © 2018 RockerHX. All rights reserved.
+//
+//  GitHub: https://github.com/rockerhx
+//
+
+
+import UIKit
+
+
+@IBDesignable class HXIBTextField: UITextField {}
+
