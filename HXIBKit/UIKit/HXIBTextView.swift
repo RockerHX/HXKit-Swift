049c7ba7a6711892fbe46fb9294fab836c74e36d
RockerHX <rockerhx@gmail.com>
2018-12-17 15:31:15 +0800

    Create HXIBTextView.swift

---
+++ HXIBKit/UIKit/HXIBTextView.swift
@@ -0,0 +1,16 @@
+//
+//  HXIBTextView.swift
+//  HXKit-Swift
+//
+//  Created by RockerHX on 2018/12/17.
+//  Copyright © 2018 RockerHX. All rights reserved.
+//
+//  GitHub: https://github.com/rockerhx
+//
+
+
+import UIKit
+
+
+@IBDesignable class HXIBTextView: UITextView {}
+
