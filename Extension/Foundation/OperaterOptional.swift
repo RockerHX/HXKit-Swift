f249e674d1dd873c1a28bfb38ff8bb87df935d42
RockerHX <rockerhx@gmail.com>
2019-05-08 18:05:18 +0800

    Update OperaterOptional.swift

--- Extension/Foundation/OperaterOptional.swift
+++ Extension/Foundation/OperaterOptional.swift
@@ -415,21 +415,6 @@ extension Optional where Wrapped == IntegerLiteralType {
 }
 
 
-// MARK: - Float Literal Type -
-extension Optional where Wrapped == FloatLiteralType {
-
-    var string: String? {
-        switch self {
-        case .some(let value):
-            return String(value)
-        default:
-            return nil
-        }
-    }
-
-}
-
-
 // MARK: - String Literal Type -
 extension Optional where Wrapped == StringLiteralType {
 
@@ -462,4 +447,703 @@ extension Optional where Wrapped == StringLiteralType {
 
 }
 
+
+// MARK: - Float Literal Type -
+extension Optional where Wrapped == FloatLiteralType {
+
+    var string: String? {
+        switch self {
+        case .some(let value):
+            return String(value)
+        default:
+            return nil
+        }
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func + (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Wrapped {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value + rhs
+            }
+            if let value = rhs {
+                return lhs + value
+            }
+            return 0
+        }
+        return lhsValue + rhsValue
+    }
+
+    static func + (lhs: Optional<Wrapped>, rhs: Wrapped) -> Wrapped {
+        if let value = lhs {
+            return value + rhs
+        }
+        return rhs
+    }
+
+    static func + (lhs: Wrapped, rhs: Optional<Wrapped>) -> Wrapped {
+        if let value = rhs {
+            return lhs + value
+        }
+        return lhs
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func - (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Wrapped {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value - rhs
+            }
+            if let value = rhs {
+                return lhs - value
+            }
+            return 0
+        }
+        return lhsValue - rhsValue
+    }
+
+    static func - (lhs: Optional<Wrapped>, rhs: Wrapped) -> Wrapped {
+        if let value = lhs {
+            return value - rhs
+        }
+        return -rhs
+    }
+
+    static func - (lhs: Wrapped, rhs: Optional<Wrapped>) -> Wrapped {
+        if let value = rhs {
+            return lhs - value
+        }
+        return lhs
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func * (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Wrapped {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value * rhs
+            }
+            if let value = rhs {
+                return lhs * value
+            }
+            return 0
+        }
+        return lhsValue * rhsValue
+    }
+
+    static func * (lhs: Optional<Wrapped>, rhs: Wrapped) -> Wrapped {
+        if let value = lhs {
+            return value * rhs
+        }
+        return 0
+    }
+
+    static func * (lhs: Wrapped, rhs: Optional<Wrapped>) -> Wrapped {
+        if let value = rhs {
+            return lhs * value
+        }
+        return 0
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func / (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Optional<Wrapped> {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value / rhs
+            }
+            if let value = rhs {
+                return lhs / value
+            }
+            return nil
+        }
+        return lhsValue / rhsValue
+    }
+
+    static func / (lhs: Optional<Wrapped>, rhs: Wrapped) -> Optional<Wrapped> {
+        if 0 == rhs {
+            return nil
+        }
+        if let value = lhs {
+            return Optional(value / rhs)
+        }
+        return nil
+    }
+
+    static func / (lhs: Wrapped, rhs: Optional<Wrapped>) -> Optional<Wrapped> {
+        if let value = rhs {
+            if 0 == value {
+                return nil
+            }
+            return Optional(lhs / value)
+        }
+        return nil
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func += (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs + rhs
+    }
+
+    static func += (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs + rhs
+    }
+
+    static func += (lhs: inout Wrapped, rhs: Optional<Wrapped>) {
+        lhs = lhs + rhs
+    }
+
+    static func -= (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs - rhs
+    }
+
+    static func -= (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs - rhs
+    }
+
+    static func -= (lhs: inout Wrapped, rhs: Optional<Wrapped>) {
+        lhs = lhs - rhs
+    }
+
+    static func *= (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs * rhs
+    }
+
+    static func *= (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs * rhs
+    }
+
+    static func *= (lhs: inout Wrapped, rhs: Optional<Wrapped>) {
+        lhs = lhs * rhs
+    }
+
+    static func /= (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs / rhs
+    }
+
+    static func /= (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs / rhs
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func == (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        if let left = lhs, let right = rhs {
+            return left == right
+        }
+        return false
+    }
+
+    static func == (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value == rhs
+        }
+        return false
+    }
+
+    static func == (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs == value
+        }
+        return false
+    }
+
+    static func != (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value != rhs
+            }
+            if let value = rhs {
+                return lhs != value
+            }
+            return false
+        }
+        return lhsValue != rhsValue
+    }
+
+    static func != (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value != rhs
+        }
+        return true
+    }
+
+    static func != (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs != value
+        }
+        return true
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func > (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        if let left = lhs, let right = rhs {
+            return left > right
+        }
+        return false
+    }
+
+    static func > (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value > rhs
+        }
+        return false
+    }
+
+    static func > (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs > value
+        }
+        return true
+    }
+
+    static func >= (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value >= rhs
+            }
+            if let value = rhs {
+                return lhs >= value
+            }
+            return false
+        }
+        return lhsValue >= rhsValue
+    }
+
+    static func >= (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value >= rhs
+        }
+        return false
+    }
+
+    static func >= (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs >= value
+        }
+        return false
+    }
+
+}
+
+
+extension Optional where Wrapped == FloatLiteralType {
+
+    static func < (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        if let left = lhs, let right = rhs {
+            return left < right
+        }
+        return false
+    }
+
+    static func < (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value < rhs
+        }
+        return true
+    }
+
+    static func < (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs < value
+        }
+        return false
+    }
+
+    static func <= (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value <= rhs
+            }
+            if let value = rhs {
+                return lhs <= value
+            }
+            return false
+        }
+        return lhsValue <= rhsValue
+    }
+
+    static func <= (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value <= rhs
+        }
+        return false
+    }
+
+    static func <= (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs <= value
+        }
+        return false
+    }
+
+}
+
+
+// MARK: - CGFloat Type -
+extension Optional where Wrapped == CGFloat {
+
+    static func + (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Wrapped {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value + rhs
+            }
+            if let value = rhs {
+                return lhs + value
+            }
+            return 0
+        }
+        return lhsValue + rhsValue
+    }
+
+    static func + (lhs: Optional<Wrapped>, rhs: Wrapped) -> Wrapped {
+        if let value = lhs {
+            return value + rhs
+        }
+        return rhs
+    }
+
+    static func + (lhs: Wrapped, rhs: Optional<Wrapped>) -> Wrapped {
+        if let value = rhs {
+            return lhs + value
+        }
+        return lhs
+    }
+
+}
+
+
+extension Optional where Wrapped == CGFloat {
+
+    static func - (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Wrapped {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value - rhs
+            }
+            if let value = rhs {
+                return lhs - value
+            }
+            return 0
+        }
+        return lhsValue - rhsValue
+    }
+
+    static func - (lhs: Optional<Wrapped>, rhs: Wrapped) -> Wrapped {
+        if let value = lhs {
+            return value - rhs
+        }
+        return -rhs
+    }
+
+    static func - (lhs: Wrapped, rhs: Optional<Wrapped>) -> Wrapped {
+        if let value = rhs {
+            return lhs - value
+        }
+        return lhs
+    }
+
+}
+
+
+extension Optional where Wrapped == CGFloat {
+
+    static func * (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Wrapped {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value * rhs
+            }
+            if let value = rhs {
+                return lhs * value
+            }
+            return 0
+        }
+        return lhsValue * rhsValue
+    }
+
+    static func * (lhs: Optional<Wrapped>, rhs: Wrapped) -> Wrapped {
+        if let value = lhs {
+            return value * rhs
+        }
+        return 0
+    }
+
+    static func * (lhs: Wrapped, rhs: Optional<Wrapped>) -> Wrapped {
+        if let value = rhs {
+            return lhs * value
+        }
+        return 0
+    }
+
+}
+
+
+extension Optional where Wrapped == CGFloat {
+
+    static func / (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Optional<Wrapped> {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value / rhs
+            }
+            if let value = rhs {
+                return lhs / value
+            }
+            return nil
+        }
+        return lhsValue / rhsValue
+    }
+
+    static func / (lhs: Optional<Wrapped>, rhs: Wrapped) -> Optional<Wrapped> {
+        if 0 == rhs {
+            return nil
+        }
+        if let value = lhs {
+            return Optional(value / rhs)
+        }
+        return nil
+    }
+
+    static func / (lhs: Wrapped, rhs: Optional<Wrapped>) -> Optional<Wrapped> {
+        if let value = rhs {
+            if 0 == value {
+                return nil
+            }
+            return Optional(lhs / value)
+        }
+        return nil
+    }
+
+}
+
+
+extension Optional where Wrapped == CGFloat {
+
+    static func += (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs + rhs
+    }
+
+    static func += (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs + rhs
+    }
+
+    static func += (lhs: inout Wrapped, rhs: Optional<Wrapped>) {
+        lhs = lhs + rhs
+    }
+
+    static func -= (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs - rhs
+    }
+
+    static func -= (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs - rhs
+    }
+
+    static func -= (lhs: inout Wrapped, rhs: Optional<Wrapped>) {
+        lhs = lhs - rhs
+    }
+
+    static func *= (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs * rhs
+    }
+
+    static func *= (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs * rhs
+    }
+
+    static func *= (lhs: inout Wrapped, rhs: Optional<Wrapped>) {
+        lhs = lhs * rhs
+    }
+
+    static func /= (lhs: inout Optional<Wrapped>, rhs: Optional<Wrapped>) {
+        lhs = lhs / rhs
+    }
+
+    static func /= (lhs: inout Optional<Wrapped>, rhs: Wrapped) {
+        lhs = lhs / rhs
+    }
+
+}
+
+
+extension Optional where Wrapped == CGFloat {
+
+    static func == (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        if let left = lhs, let right = rhs {
+            return left == right
+        }
+        return false
+    }
+
+    static func == (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value == rhs
+        }
+        return false
+    }
+
+    static func == (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs == value
+        }
+        return false
+    }
+
+    static func != (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value != rhs
+            }
+            if let value = rhs {
+                return lhs != value
+            }
+            return false
+        }
+        return lhsValue != rhsValue
+    }
+
+    static func != (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value != rhs
+        }
+        return true
+    }
+
+    static func != (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs != value
+        }
+        return true
+    }
+
+}
+
+
+extension Optional where Wrapped == CGFloat {
+
+    static func > (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        if let left = lhs, let right = rhs {
+            return left > right
+        }
+        return false
+    }
+
+    static func > (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value > rhs
+        }
+        return false
+    }
+
+    static func > (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs > value
+        }
+        return true
+    }
+
+    static func >= (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value >= rhs
+            }
+            if let value = rhs {
+                return lhs >= value
+            }
+            return false
+        }
+        return lhsValue >= rhsValue
+    }
+
+    static func >= (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value >= rhs
+        }
+        return false
+    }
+
+    static func >= (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs >= value
+        }
+        return false
+    }
+
+}
+
+
+extension Optional where Wrapped == CGFloat {
+
+    static func < (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        if let left = lhs, let right = rhs {
+            return left < right
+        }
+        return false
+    }
+
+    static func < (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value < rhs
+        }
+        return true
+    }
+
+    static func < (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs < value
+        }
+        return false
+    }
+
+    static func <= (lhs: Optional<Wrapped>, rhs: Optional<Wrapped>) -> Bool {
+        guard let lhsValue = lhs, let rhsValue = rhs else {
+            if let value = lhs {
+                return value <= rhs
+            }
+            if let value = rhs {
+                return lhs <= value
+            }
+            return false
+        }
+        return lhsValue <= rhsValue
+    }
+
+    static func <= (lhs: Optional<Wrapped>, rhs: Wrapped) -> Bool {
+        if let value = lhs {
+            return value <= rhs
+        }
+        return false
+    }
+
+    static func <= (lhs: Wrapped, rhs: Optional<Wrapped>) -> Bool {
+        if let value = rhs {
+            return lhs <= value
+        }
+        return false
+    }
+
+}
 
