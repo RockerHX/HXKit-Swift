a97958a7fa07d27d4f5f734eecefded262bc1e25
RockerHX <rockerhx@gmail.com>
2021-04-28 09:24:32 +0800

    Update HXDatePickerViewController.swift

--- Classes/UIKit/Picker/Date/Code/HXDatePickerViewController.swift
+++ Classes/UIKit/Picker/Date/Code/HXDatePickerViewController.swift
@@ -12,7 +12,7 @@
 import UIKit
 
 
-protocol HXDatePickerViewControllerDelegate: class {
+protocol HXDatePickerViewControllerDelegate: AnyObject {
 
     func datePickerView(_ controller: HXDatePickerViewController, selected date: Date)
     func datePickerView(_ controller: HXDatePickerViewController, comfirm date: Date)
