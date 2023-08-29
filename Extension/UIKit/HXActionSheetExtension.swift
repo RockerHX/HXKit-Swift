7ca206c99866f2b272b9edcfa1a0a6c3d1ca2459
RockerHX <rockerhx@gmail.com>
2023-08-29 11:45:31 +0800

    Update HXActionSheetExtension.swift

--- Extension/UIKit/HXActionSheetExtension.swift
+++ Extension/UIKit/HXActionSheetExtension.swift
@@ -12,6 +12,18 @@
 import UIKit
 
 
+private struct InputActionAssociationKey {
+    fileprivate static var completionHandlerWrapper = "com.rockerhx.UIAlertController.InputActionCompletionHandlerWrapper"
+}
+
+private extension UIAlertController {
+    var completionHandlerWrapper: HXClosureDecorator<(UIAlertController, [UITextField]?)> {
+        get { return associatedObject(forKey: &InputActionAssociationKey.completionHandlerWrapper) as! HXClosureDecorator<(UIAlertController, [UITextField]?)> }
+        set { associate(retainObject: newValue, forKey: &InputActionAssociationKey.completionHandlerWrapper) }
+    }
+}
+
+
 // 显示上拉选择菜单Extension
 extension UIViewController {
 
@@ -33,6 +45,21 @@ extension UIViewController {
         for action in otherActions {
             controller.addAction(action)
         }
+        present(controller, animated: true, completion: nil)
+    }
+
+    public func showInputAlert(with title: String, message: String? = nil, completionHandler: @escaping ((UIAlertController, [UITextField]?) -> ())) {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        controller.completionHandlerWrapper = HXClosureDecorator(completionHandler)
+        controller.addTextField { textField in
+            textField.placeholder = "请输入要修改的内容"
+        }
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel)
+        let confirmAction = UIAlertAction(title: "保存", style: .destructive) { _ in
+            controller.completionHandlerWrapper.invoke((controller, controller.textFields))
+        }
+        controller.addAction(cancelAction)
+        controller.addAction(confirmAction)
         present(controller, animated: true, completion: nil)
     }
     
